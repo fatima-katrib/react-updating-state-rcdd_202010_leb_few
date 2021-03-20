9b829aaa696c47b46a76debf46d769c710607b45
jovial-application-1943 <fatima.katrib@lau.edu>
2021-03-20 13:12:53 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,13 @@
 // Code ClickityClick Component Here
+import React from 'react';
+export default class ClickityClick extends React.Component {
+  
+handleClick = () => {
+  this.setState({
+    hasBeenClicked: true
+  }, () => console.log(this.state.hasBeenClicked))
+  
+ render() {
+   
+ }
+}
